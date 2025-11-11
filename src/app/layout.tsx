7295249c5113e2508e632361859760c150c543f7
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-11-11 13:13:27 -0300

    chore: update site URL and Open Graph image for consistency across platforms

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,27 +13,28 @@ const lexendZetta = Lexend_Zetta({
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://evro.my"),
+	metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://www.evro.finance"),
 	title: "EVRO",
 	description: "An open-source protocol that maintains synthetic value units through fully decentralized smart-contract mechanics.",
 	openGraph: {
+		url: "https://www.evro.finance/",
+		type: "website",
 		title: "EVRO",
 		description: "An open-source protocol that maintains synthetic value units through fully decentralized smart-contract mechanics.",
 		images: [
 			{
-				url: "/assets/og-image.png",
+				url: "https://opengraph.b-cdn.net/production/images/16ffb740-ca48-493d-a677-731c82273a35.png?token=jijjvjEDRCZwBMk6yWXRiMEg6rJBfl0rS5ixeBUIzkg&height=630&width=1200&expires=33298877443",
 				width: 1200,
 				height: 630,
-				alt: "EVRO",
+				alt: "EVRO Protocol",
 			},
 		],
-		type: "website",
 	},
 	twitter: {
 		card: "summary_large_image",
 		title: "EVRO",
 		description: "An open-source protocol that maintains synthetic value units through fully decentralized smart-contract mechanics.",
-		images: ["/assets/og-image.png"],
+		images: ["https://opengraph.b-cdn.net/production/images/16ffb740-ca48-493d-a677-731c82273a35.png?token=jijjvjEDRCZwBMk6yWXRiMEg6rJBfl0rS5ixeBUIzkg&height=630&width=1200&expires=33298877443"],
 	},
 };
 
